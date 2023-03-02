46ba9d3663f12c5b527a6543f5f6d8c3fac031f4
Lucas Charoing <charoing.lucas@gmail.com>
2023-03-02 17:29:51 +0100

    Add adding and deleting tasks functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {useContext, useState} from "react";
 import './App.css'
+import { TasksContext } from "./contexts/tasksContext";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { tasks, addTask, deleteTask } = useContext(TasksContext);
+  const [taskTitle, setTaskTitle] = useState("");
+
+  const handleChange = (e) => {
+    setTaskTitle(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTask({ id: Date.now(), title: taskTitle });
+    setTaskTitle("");
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Tasks</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder={"Task title"} value={taskTitle} onChange={handleChange}/>
+        <button type={"submit"}>Add</button>
+      </form>
+      <ul>
+          {tasks.map((task) => (
+              <li key={task.id}>
+                  {task.title}
+                  <button onClick={() => deleteTask(task.id)}>Delete</button>
+              </li>
+          ))}
+      </ul>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { TasksProvider} from "./contexts/tasksContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <TasksProvider>
+      <App />
+    </TasksProvider>
   </React.StrictMode>,
 )
